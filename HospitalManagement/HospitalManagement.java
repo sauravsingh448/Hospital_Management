fc8e45b8abccfe77b2767d26a42d9371f312b59a
Saurav singh <sauravsinghk842@gmail.com>
2025-05-02 11:45:41 +0530

    Update HospitalManagement.java

--- HospitalManagement/HospitalManagement.java
+++ HospitalManagement/HospitalManagement.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class HospitalManagement {
    private static final String url = "jdbc:mysql://localhost:3306/hospital";
    private static final String username = "root";
-   private static final String password = "Root@123";
+   private static final String password = "?";
 
    public static void main(String[] args) {
       try{
